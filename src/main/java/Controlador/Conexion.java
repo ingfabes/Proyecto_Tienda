7eabcb5e65aeb2a68b8a2b043bd6918c95bae6f3
root <root@augusto.melo>
2021-10-22 20:45:22 -0500

    conexión a la nueva base de datos MARIADB

--- src/main/java/Controlador/Conexion.java
+++ src/main/java/Controlador/Conexion.java
@@ -3,19 +3,22 @@ package Controlador;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import javax.swing.JOptionPane;
+
 public class Conexion {
-	private String bd="tiendagenerica";
-	private String url="jdbc:mysql://localhost:3306/"+bd;
-	private String user="root";
-	private String pass="admin";
+	private String bd="grupo25_equipo5";
+	private String url="jdbc:mariadb://grupo25db2.c47knbsonjdi.us-east-2.rds.amazonaws.com:3306/"+bd;//"jdbc:mysql://localhost:3306/"+bd;
+	private String user="admin";
+	private String pass="Grupo25DB2";
 	
 	Connection conect=null;
 	
 	public Connection conecta() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 			conect=DriverManager.getConnection(url,user,pass);
 		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "error de conexion:" + e);
 			conect=null;
 		}
 		return conect;
